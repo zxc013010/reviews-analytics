0ee5faff964b05fa3c1de8c75a291b0bd9627276
Bill Lin <bill0841205@gmail.com>
2021-01-27 17:32:02 +0800

    finished

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -13,3 +13,17 @@ for d in data:
 	sum_len += len(d)
 print(len(data), '筆資料的總長度為:', sum_len)
 print('每筆留言平均長度為:', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0:5])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
